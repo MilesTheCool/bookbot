5fffdce3c9ecf98f2ade43adfbb7e34146e9c384
Miles Rasmussen <miles.alan.raz@gmail.com>
2024-07-25 00:29:27 -0700

    E: added function to count chars, moved file read to new function

--- main.py
+++ main.py
@@ -3,14 +3,30 @@
 """
 
 def main():
-    with open("/home/miles/Documents/Programming/Python/bookbot/books/short.txt") as f:
-        file_contents = f.read()
-            
-        print(f"There are {word_count(file_contents)} words!")
-        
+    path = "/home/miles/Documents/Programming/Python/bookbot/books/short.txt"
+    file_contents = get_file_text(path)
+    print(f"There are {word_count(file_contents)} words!")
+    
+    print("\n\nCharacter Count: ", character_count(file_contents)) 
+    
 def word_count(contents):
     return len(contents.split())
+
+def get_file_text(path):
+    with open(path) as f:
+        return f.read()
     
+def character_count(contents):
+    contents_chars = str(contents)
+    contents_dict = {}
+
+    for char in contents_chars:
+        if char in contents_dict.keys():
+            contents_dict[char] += 1
+        else:
+            contents_dict[char] = 1
+    
+    return contents_dict
 
 if __name__ == "__main__":
     main()
