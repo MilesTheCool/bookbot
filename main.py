1f84a4848c031d941a882c1e24d5ad95befa0be2
Miles Rasmussen <miles.alan.raz@gmail.com>
2024-07-25 01:06:31 -0700

    F: Separated char counting into 3 groups, alphabet, digit, and other. Each group was made into separate dictionary by rewritting cout char. A new function was then needed to sort them as a list of dictionaries most occurences to least Finally, the count of each individual charcater is displayed to the user, separated by category

--- main.py
+++ main.py
@@ -5,10 +5,25 @@
 def main():
     path = "/home/miles/Documents/Programming/Python/bookbot/books/short.txt"
     file_contents = get_file_text(path)
-    print(f"There are {word_count(file_contents)} words!")
+    print(f"There are {word_count(file_contents)} words!\n")
     
-    print("\n\nCharacter Count: ", character_count(file_contents)) 
     
+    alphabet, digits, special = character_count(file_contents)
+    
+    print("Alphabet:")
+    [print(f"\tThe '{sub_dict['char']}' character was found {sub_dict['num']} times") for sub_dict in alphabet]
+    
+    print("\nDigits: ")
+    [print(f"\tThe '{sub_dict['char']}' character was found {sub_dict['num']} times") for sub_dict in digits]
+
+    print("\nSpecial Chars: ")
+    for sub_dict in special:
+        char = sub_dict['char']
+        if char == '\t': char = "\\t"
+        elif char == '\n': char = "\\n"
+        print(f"\tThe '{char}' character was found {sub_dict['num']} times")
+
+        
 def word_count(contents):
     return len(contents.split())
 
@@ -18,15 +33,48 @@ def get_file_text(path):
     
 def character_count(contents):
     contents_chars = str(contents)
-    contents_dict = {}
-
-    for char in contents_chars:
-        if char in contents_dict.keys():
-            contents_dict[char] += 1
-        else:
-            contents_dict[char] = 1
+    alphabet_dict = {}
+    number_dict = {}
+    character_dict = {}
     
-    return contents_dict
+    # get the count for each letter, char, and num differently
+    for char in contents_chars:
+        char = char.lower()
+        
+        # check alphabet
+        if char in ['a','b','c','d','e','f','g','h','i','j','k','l',
+                    'm','n','o','p','q','r','s','t','u','v','w',
+                    'x','y','z']:
+            if char in alphabet_dict.keys():
+                alphabet_dict[char] += 1
+            else:
+                alphabet_dict[char] = 1
+                
+        # number
+        elif char in ['1','2','3','4','5','6','7','8','9','0']:
+            if char in number_dict.keys():
+                number_dict[char] += 1
+            else:
+                number_dict[char] = 1
+            
+        # special character
+        else:
+            if char in character_dict.keys():
+                character_dict[char] += 1
+            else:
+                character_dict[char] = 1
+    
+    
+    return sort_dict(alphabet_dict), sort_dict(number_dict), sort_dict(character_dict)
+
+def sort_dict(dictionary):
+    # convert into list of dictionaries in the following format
+    # dictionary = [
+    #  { "char" : key, "num" : value}   
+    #]
+    new_dict = [{"char" : key, "num" : dictionary[key]} for key in dictionary.keys()]
+    new_dict.sort(reverse=True, key=lambda x : x["num"])
+    return new_dict
 
 if __name__ == "__main__":
     main()
