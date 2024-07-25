076f040e840c34ccaa3324aaad38f27df6f3eee9
Miles Rasmussen <miles.alan.raz@gmail.com>
2024-07-25 00:15:38 -0700

    D: added function for word count

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def main():
     with open("/home/miles/Documents/Programming/Python/bookbot/books/short.txt") as f:
         file_contents = f.read()
             
-        print(file_contents)
+        print(f"There are {word_count(file_contents)} words!")
+        
+def word_count(contents):
+    return len(contents.split())
+    
 
 if __name__ == "__main__":
     main()
